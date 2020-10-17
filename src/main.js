e0cfdaebd18506cbfb0a817a0e515db4acc4ff6d
getzch <keepprogress@gmail.com>
2020-10-17 14:28:46 +0800

    complete login page & dashboard & loading animation

---
+++ src/main.js
@@ -0,0 +1,51 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import axios from 'axios';
+import 'bootstrap';
+
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import router from './router';
+
+
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = false;
+
+Vue.component('Loading', Loading);
+axios.defaults.withCredentials = true;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router,
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('to', to, from,'next', next)
+  // ...
+  if (to.meta.requiresAuth){
+    const api = `${process.env.APIPATH}/api/user/check`;
+      const vm = this;
+      console.log(process.env.APIPATH, process.env.CUSTOMPATH);
+      axios.post(api,).then((response) => {
+        //axios.post(api,) 替換 this.$router.post(api,) 
+        // 因為這邊是router 元件才能使用 this.$router.post
+        console.log(response.data);
+        if (response.data.success) {
+            // vm.$router.push('/');
+            next();
+        } else {
+          next({
+            paath: '/login',
+          })
+        }
+      });
+  } else {
+    next();
+  };
+});
---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+//官方元件
+
+// import Home from '@/components/HelloWorld';
+import Dashboard from '@/components/Dashboard';
+import Login from '@/components/Pages/Login';
+import Products from '@/components/Pages/Products';
+
+//自訂的元件
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '*',
+            redirect: 'login',
+        },
+        // {
+        //     name: '首頁',
+        //     path: '/',
+        //     component: Home,
+        //     meta: { requiresAuth: true },
+        // },
+        {
+            name: 'Login',
+            path: '/login',
+            component: Login,
+        },
+        {
+            name: '首頁',
+            path: '/admin',
+            component: Dashboard,
+            
+            children: [
+                {
+                    path: 'products',
+                    name: 'Products',
+                    component: Products,
+                    meta: { requiresAuth: true },
+                },
+            ],
+        },
+
+    ],
+
+});
