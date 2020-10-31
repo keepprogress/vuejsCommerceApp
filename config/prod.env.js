b073d6964314dfedf949ad1cfb9d3d5396eabc75
getzch <keepprogress@gmail.com>
2020-10-31 18:48:58 +0800

    fix config prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
-module.exports = {
-  NODE_ENV: '"production"'
-}
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io"',
+  CUSTOMPATH: '"getzch"'
+
+});
